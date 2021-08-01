f89fdbaed73b20ddbcc00cbe6c39831a2eb6837d
Todd Hogarth <begin-again@users.noreply.github.com>
2021-08-01 10:29:31 -0400

    init

---
+++ one.js
@@ -0,0 +1,11 @@
+/* CHALLENGE
+
+To proceed, write a program that prints "i am okay" to standard output
+and "i am so incredibly not okay" to standard error.
+
+
+To verify your program, run: `bug-clinic verify program.js`.
+*/
+
+console.log("i am okay")
+console.error("i am so incredibly not okay")
